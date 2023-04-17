0c7ad1e4a6a089ddcab2dbaefa2c50bbce8cb5b7
Livia Silva Santos <liviasilvasantos@gmail.com>
2023-04-17 19:01:21 -0300

    add annotations

--- localstack/localstack.sh
+++ localstack/localstack.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-# cria parameter
+# cria parameter - parameter store
 aws --endpoint http://localhost:4566 --profile localstack ssm put-parameter --name "/config/spring-boot-localstack_localstack/helloWorld" --value "Hello World Parameter Store" --type String
 
-# cria secrets
+# cria secrets - secrets manager
 aws --endpoint http://localhost:4566 --profile localstack secretsmanager create-secret --name /secret/spring-boot-localstack_localstack --description "Exemplo de Secrets Manager" --secret-string "{\"valor1\":\"Oi Mundo\",\"valor2\":\"Hello World\",\"valor3\":\"Hola Mundo\"}"
 aws --endpoint http://localhost:4566 --profile localstack secretsmanager create-secret --name /secret/spring-boot-localstack --description "Exemplo de Secrets Manager" --secret-string "{\"valor1\":\"Oi Mundo\",\"valor2\":\"Hello World\",\"valor3\":\"Hola Mundo\"}"
 aws --endpoint http://localhost:4566 --profile localstack secretsmanager create-secret --name /secret/application --description "Exemplo de Secrets Manager" --secret-string "{\"valor1\":\"Oi Mundo\",\"valor2\":\"Hello World\",\"valor3\":\"Hola Mundo\"}"
--- src/main/java/br/com/liviasilvasantos/ssm/configuration/ParameterStoreConfiguration.java
+++ src/main/java/br/com/liviasilvasantos/ssm/configuration/ParameterStoreConfiguration.java
@@ -1,7 +1,6 @@
 package br.com.liviasilvasantos.ssm.configuration;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
